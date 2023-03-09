5836238ef6d6d53d23447a6df417fe9681e06f3e
andrehaueisen <andrehaueisen@gmail.com>
2023-03-08 21:49:55 -0300

    Adds user tests

--- spec/tests/models/user.test.ts
+++ spec/tests/models/user.test.ts
@@ -1,5 +1,83 @@
-describe('Fake test', () => {
-  test('should pass', () => {
-    expect(true).toBe(true);
+import client from '../../../src/database';
+import { UserStore, User } from '../../../src/models/user';
+
+const store = new UserStore(10, 'pepper');
+
+describe('User model', () => {
+  test('should have an index method', () => {
+    expect(store.index).toBeDefined();
+  });
+
+  test('should have a show method', () => {
+    expect(store.show).toBeDefined();
+  });
+
+  test('should have a create method', () => {
+    expect(store.create).toBeDefined();
+  });
+
+  test('should have a authenticate method', () => {
+    expect(store.authenticate).toBeDefined();
+  });
+
+  test('create should add a user', async () => {
+    const userInput = {
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'password'
+    };
+
+    const userInput2 = {
+      firstName: 'Jane',
+      lastName: 'Doe',
+      password: 'password'
+    };
+
+    const result = await store.create(userInput);
+    const result2 = await store.create(userInput2);
+
+    expect(result).toEqual(new User(result.id, 'John', 'Doe', result.password));
+    expect(result2).toEqual(new User(result2.id, 'Jane', 'Doe', result2.password));
+  });
+
+  test('index should return all the users', async () => {
+    const result = await store.index();
+
+    expect(result.length).toBe(2);
+    const firstUser = result[0];
+    const secondUser = result[1];
+
+    expect(firstUser).toEqual(new User(firstUser.id, 'John', 'Doe', firstUser.password));
+    expect(secondUser).toEqual(new User(secondUser.id, 'Jane', 'Doe', secondUser.password));
+  });
+
+  test('show should return the correct user', async () => {
+    const users = await store.index();
+    const firstUser = users[0];
+
+    const result = await store.show(firstUser.id);
+
+    expect(result).toEqual(new User(result.id, 'John', 'Doe', result.password));
+    expect(result).toEqual(firstUser);
+  });
+
+  test('authenticate should return a user if the password is correct', async () => {
+    const users = await store.index();
+    const firstUser = users[0];
+
+    const result = await store.authenticate(firstUser.id, 'password');
+    expect(result).toEqual(new User(firstUser.id, 'John', 'Doe', firstUser.password));
+  });
+
+  test('authenticate should throw an error if the password is incorrect', async () => {
+    const users = await store.index();
+    const firstUser = users[0];
+
+    const result = await store.authenticate(firstUser.id, 'wrongpassword');
+    expect(result).toBeNull();
+  });
+
+  afterAll(async () => {
+    await client.end();
   });
 });
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import { Store } from './store';
+import bcrypt from 'bcrypt';
 
 export class User {
   constructor(readonly id: string, readonly firstName: string, readonly lastName: string, readonly password: string) {}
@@ -22,8 +23,23 @@ export type UserInput = {
 };
 
 export class UserStore extends Store {
-  constructor(readonly saltRounds: number, readonly pepper: string, readonly jwtSecret: string) {
+  constructor(readonly saltRounds: number, readonly pepper: string) {
     super();
+  }
+
+  async index(): Promise<User[]> {
+    const conn = await super.connectToDB();
+
+    try {
+      const sql = 'SELECT * FROM users';
+      const result = await conn.query(sql);
+
+      return result.rows.map(User.fromRow);
+    } catch {
+      throw new Error('Unable to get users');
+    } finally {
+      conn.release();
+    }
   }
 
   async show(id: string): Promise<User> {
@@ -40,6 +56,45 @@ export class UserStore extends Store {
       throw new Error('User not found');
     } catch (err) {
       throw new Error(`Unable to get user ${id}: ${err}`);
+    } finally {
+      conn.release();
+    }
+  }
+
+  async create(userInput: UserInput): Promise<User> {
+    const conn = await super.connectToDB();
+
+    try {
+      const sql = 'INSERT INTO users (first_name, last_name, password) VALUES($1, $2, $3) RETURNING *';
+      const passwordHash = bcrypt.hashSync(userInput.password + this.pepper, this.saltRounds);
+
+      const result = await conn.query(sql, [userInput.firstName, userInput.lastName, passwordHash]);
+
+      return User.fromRow(result.rows[0]);
+    } catch (err) {
+      throw new Error(`Unable to create user: ${err}`);
+    } finally {
+      conn.release();
+    }
+  }
+
+  async authenticate(id: string, password: string): Promise<User | null> {
+    const conn = await super.connectToDB();
+
+    try {
+      const sql = 'SELECT * FROM users WHERE id=($1)';
+      const result = await conn.query(sql, [id]);
+
+      if (result.rows.length) {
+        const user = User.fromRow(result.rows[0]);
+        if (bcrypt.compareSync(password + this.pepper, user.password)) {
+          return user;
+        }
+      }
+
+      return null;
+    } catch (err) {
+      throw new Error(`Unable to authenticate user ${id}. ${err}`);
     } finally {
       conn.release();
     }
